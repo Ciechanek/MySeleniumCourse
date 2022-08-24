e9ef3319df5e7e1d0557a2039b0ae758cf27a1fb
Mateusz Ciechan <Mateusz_Ciechan@edwards.com>
2022-08-24 10:47:46 +0200

    test gitignore2

---
+++ MySeleniumCourse/Tests/QuitSesionAndBrowserWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MySeleniumCourse.Tests
+{
+    public class QuitSesionAndBrowserWindow : BaseTest
+    {
+        [Test]
+        public void QuitSessionAndBrowser()
+        {
+            webDriver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+            webDriver.Navigate().GoToUrl("https://pl.wikipedia.org/wiki/Wikipedia:Strona_g%C5%82%C3%B3wna");
+            webDriver.Navigate().GoToUrl("https://www.nasa.gov/");
+            webDriver.Navigate().Back();
+            webDriver.Navigate().Forward();
+            Assert.AreEqual("https://www.nasa.gov/", webDriver.Url, "Url of the page should be https://www.nasa.gov/, but is " + webDriver.Url);
+            webDriver.Close();
+            webDriver.Quit();
+        }
+    }
+}
---
+++ MySeleniumCourse/Tests/SourceAndUrlTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MySeleniumCourse.Tests
+{
+    public class SourceAndUrlTests : BaseTest
+    {
+        [Test]
+        public void GetSourceAndTitleOfThePageTest()
+        {
+            webDriver.Navigate().GoToUrl("https://www.saucedemo.com/");
+            Assert.AreEqual("https://www.saucedemo.com/", webDriver.Url, "Url of the page should be https://www.saucedemo.com/, but is " + webDriver.Url);
+            Assert.That(webDriver.Title, Is.EqualTo("Swag Labs"), "Title of the page is invalid");
+            //testline
+        }
+    }
+}
