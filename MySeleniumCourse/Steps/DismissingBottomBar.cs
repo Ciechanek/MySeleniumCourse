41fa88d819c4039af12b432516ff4d9cfb37791e
Mateusz Ciechan <Mateusz_Ciechan@edwards.com>
2022-11-17 13:38:35 +0100

    XPathTests

---
+++ MySeleniumCourse/Steps/DismissingBottomBar.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySeleniumCourse.Steps
+{
+    public class DismissingBottomBar
+    {
+        public void CloseBottomBar(WebDriver driver)
+        {
+            driver.FindElement(By.CssSelector("a[class='woocommerce-store-notice__dismiss-link']")).Click();
+        }
+    }
+}
---
+++ MySeleniumCourse/Tests/XPathTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using MySeleniumCourse.Steps;
+using System.Threading;
+
+namespace MySeleniumCourse.Tests
+{
+    public class XPathTests : BaseTest
+    {
+        private AddingItemsToCart addingItemsToCart = new AddingItemsToCart();
+        private DismissingBottomBar dismissingBottomBar = new DismissingBottomBar();
+        private CartOperations cartOperations = new CartOperations();
+        private PaymentOperations paymentOperations = new PaymentOperations();
+
+        [Test]
+        public void TestsOfXPaths()
+        {
+            webDriver.Navigate().GoToUrl("https://fakestore.testelka.pl");
+            dismissingBottomBar.CloseBottomBar(webDriver);
+            addingItemsToCart.AddToCartSotaventaLimnos(webDriver);
+            Thread.Sleep(2000);
+            cartOperations.SelectCartIcon(webDriver);
+            paymentOperations.SelectCheckoutButton(webDriver);
+            paymentOperations.FindFirstProductNameOnCheckout(webDriver);
+        }
+    }
+}
